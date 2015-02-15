c1898df04c18fb095da46c080ed588232bd57c60
Will O'Brien <will.obrien@dibit.co>
2015-02-15 11:47:16 -0500

    Print project headers

--- src/toMd.py
+++ src/toMd.py
@@ -12,14 +12,18 @@ from sh import pandoc
 
 if __name__ == "__main__":
   for line in sys.stdin:
-    project = json.loads(line)
+    projects = json.loads(line)
 
-    project_name = "# " + project["name"]
-    for chunk in pandoc(["-f", "markdown", "-t", "json"], _in=project_name ):
-      header = json.loads(chunk)[1][0]
-      sys.stderr.write(str(header) + "\n#####")
+    headers = []
+    for project in projects:
+      project_name = "# " + project["name"]
+      for chunk in pandoc(["-f", "markdown", "-t", "json"], _in=project_name ):
+        header = json.loads(chunk)[1][0]
+        headers.append(header)
+        sys.stderr.write(str(header) + "\n#####\n")
 
-    doc = [{"unMeta": {}}, [header]]
+    doc = [{"unMeta": {}}, headers]
     json.dump(doc, sys.stdout)
+
   sys.stdout.flush()
   sys.stdout.close()
